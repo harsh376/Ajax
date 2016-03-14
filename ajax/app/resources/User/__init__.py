15082faf29bfd1703145984a8d94c5c861409513
Harsh Verma <harsh376@gmail.com>
2016-03-13 21:00:25 -0400

    - set up alembic, accepting post requests

--- ajax/app/resources/User/__init__.py
+++ ajax/app/resources/User/__init__.py
@@ -1,10 +1,17 @@
 from flask import make_response
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from app.utils import row_to_dict
 from .models import User
+
+parser = reqparse.RequestParser()
+parser.add_argument('some')
 
 
 class UserResource(Resource):
     def get(self):
         user = row_to_dict(User.query.first())
         return make_response(user['name'])
+
+    def post(self):
+        args = parser.parse_args()
+        return make_response(args['some'] + 'wooo')
