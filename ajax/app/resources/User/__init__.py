98218350a901e0c8a16989b31390f0ddf88b61b9
Harsh Verma <harsh376@gmail.com>
2016-03-15 00:21:04 -0400

    - POST request stores data in db for `User` resource

--- ajax/app/resources/User/__init__.py
+++ ajax/app/resources/User/__init__.py
@@ -1,17 +1,29 @@
-from flask import make_response
+from flask import make_response, jsonify
 from flask_restful import Resource, reqparse
+
+from sqlalchemy.exc import IntegrityError
+
+from app import db
 from app.utils import row_to_dict
 from .models import User
 
 parser = reqparse.RequestParser()
-parser.add_argument('some')
+parser.add_argument('name')
 
 
 class UserResource(Resource):
     def get(self):
         user = row_to_dict(User.query.first())
-        return make_response(user['name'])
+        data = {'id': user['id'], 'name': user['name']}
+        return make_response(jsonify(data))
 
     def post(self):
         args = parser.parse_args()
-        return make_response(args['some'] + 'wooo')
+        try:
+            new_user = User(name=args['name'])
+            db.session.add(new_user)
+            db.session.commit()
+        except:
+            raise IntegrityError
+        data = {'id': new_user.id, 'name': new_user.name}
+        return make_response(jsonify(data))
