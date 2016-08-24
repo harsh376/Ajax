5b2cccdbede45aea25b79d25acbdbeab6d09b3a5
Harsh Verma <harsh376@gmail.com>
2016-08-25 01:20:30 +0530

    WD-74 Updated docker-compose file for dev flow

--- ajax/script.sh
+++ ajax/script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 cmd="python run.py"
+host="$1"
+port="3306"
+user="$2"
+password="$3"
 
-while ! mysql -h"192.168.99.100" -P"3306" -uroot -p"my-secret-pw"  -e ";" ; do
+while ! mysql -h"$host" -P"$port" -u"$user" -p"$password"  -e ";" ; do
     >&2 echo "Can't connect, please retry"
     sleep 1
 done
-
 
 >&2 echo "MYSQL is up - executing command"
 exec $cmd
